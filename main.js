0126519cc9f9424cbe7f59192e569abd5ff9034b
umbank <131064364+umbank@users.noreply.github.com>
2023-04-20 15:56:48 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 function check(form) {
 
   if (form.userid.value == "admin" && form.pwd.value == "noahgrocery"){
-      form.action = "home.html";
+      form.action = "https://sites.google.com/view/unitedmustangbank/admin";
       return true;
   }
   else if(form.userid.value == "" && form.pwd.value == ""){
-      form.action = "home.html";
+      form.action = "page";
       return true;
   } else {
     alert("Incorrect Password or Username")
